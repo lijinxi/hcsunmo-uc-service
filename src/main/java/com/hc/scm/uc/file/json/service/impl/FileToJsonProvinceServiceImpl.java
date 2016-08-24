2660a35c11d965b8184dbc5389a3eebcde3ee578
lijinxi <1767270730@qq.com>
2016-08-24 23:33:36 +0800

    20160824

--- src/main/java/com/hc/scm/uc/file/json/service/impl/FileToJsonProvinceServiceImpl.java
+++ src/main/java/com/hc/scm/uc/file/json/service/impl/FileToJsonProvinceServiceImpl.java
@@ -1,9 +1,15 @@
 package com.hc.scm.uc.file.json.service.impl;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hc.scm.common.base.dal.BaseCrudDao;
 import com.hc.scm.common.base.service.BaseCrudServiceImpl;
 import com.hc.scm.uc.dao.file.json.model.Province;
@@ -21,8 +27,36 @@ public class FileToJsonProvinceServiceImpl extends BaseCrudServiceImpl implement
 
 	@Override
 	public List<Province> getProvinceList() {
-		// TODO Auto-generated method stub
-		return null;
+		String fullFileName = "D:\\hcconf\\scm\\hc-uc\\json\\provinces.json";
+	    List<Province>  list=new ArrayList<Province>();
+    	File file = new File(fullFileName);
+    	Scanner scanner = null;
+    	StringBuilder buffer = new StringBuilder();
+    	try {
+    	    scanner = new Scanner(file, "utf-8");
+    	    while (scanner.hasNextLine()) {
+    	        buffer.append(scanner.nextLine());
+    	    }
+
+    	} catch (FileNotFoundException e) {
+    	    // TODO Auto-generated catch block  
+
+    	} finally {
+    	    if (scanner != null) {
+    	        scanner.close();
+    	    }
+    	}
+    	 ObjectMapper oMapper = new ObjectMapper();
+    	 Province[]  p=null;
+    	 try {
+             p = oMapper.readValue(buffer.toString(), Province[].class);
+         } catch (Exception e) {
+             e.printStackTrace();
+         } 
+    	 for(int i=0;i<p.length;i++){
+    		 list.add(p[i]);
+    	 }
+		return list;
 	}
 
 }
